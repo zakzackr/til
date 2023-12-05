db67f2a8a1e27d7bcd3e0f77dd219b545c24b75c
Ryuichi <100734822+zakzackr@users.noreply.github.com>
2023-12-05 14:43:15 +0900

    Create scope2.java

---
+++ scope/scope2.java
@@ -0,0 +1,39 @@
+// defining A class in the global scope
+class A {
+    public static int x = 3;
+    public static int y = 10;
+
+    // The parameter x will be found in this local scope as it's a parameter of this method 
+    public static int multiply(int x){
+        // looking for y in this local scope, but cannot find it
+        // so will find y in the parent scope (y = 10)
+        return x * y;
+    }
+
+    // defining B class inside A class
+    static class B{
+        public static int x = 15;
+
+        public static int multiply(int x){
+            // looking for y in this local scope, but cannot find it
+            // so will find y in the parent scope (y = 10)
+            return x * y;
+        }
+    }
+}
+
+class Main{
+
+    public static void main(String[] args){
+        int x = 33;
+        System.out.println(x); // 33
+
+        System.out.println(A.x); // 3
+
+        System.out.println(A.multiply(5)); // 50
+
+        System.out.println(A.B.x); // 15
+
+        System.out.println(A.B.multiply(2)); // 20
+    }
+}
