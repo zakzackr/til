0f7f354d3cf1072df2150c309f546ebaebcdbde5
Ryuichi <100734822+zakzackr@users.noreply.github.com>
2023-12-25 15:53:31 +0900

    Create higher-order-function-7.java

---
+++ lambda-function/higher-order-function-7.java
@@ -0,0 +1,44 @@
+// 言語によっては関数を高階関数の入力として使用することもできるが、Javaでは単純に関数への参照をそのままでは渡すことができません。
+// 通常の関数への参照を入力として受け取るには、渡したい関数のスケルトンのみを持つinterfaceを作成し、その実装クラスを定義する必要がある。
+
+import java.util.Random;
+import java.util.function.*;
+
+interface Callable{
+    public String greetingFn(String name);
+}
+
+class Greet implements Callable{
+    public String greetingFn(String name){
+        return "Hello, " + name;
+    }
+}
+
+public class Main {
+    public static String multiCall(Function<String, String> greetFn, Supplier<String> createName, String message){
+        return greetFn.apply(createName.get()) + "..." + message;
+    }
+
+    // Callableインターフェースで通常の関数への参照を受け取る関数
+    public static String multiCall(Callable f, Supplier<String> createName, String message){
+        return f.greetingFn(createName.get()) + "..." + message;
+    }
+
+    public static void main(String[] args) {
+        Function<String, String> greetingLambda = name -> "Hello, " + name;
+        Supplier<String> createNameLambda = () -> {
+            String str = "";
+            for(int i = 0; i < 10; i++){
+                Random r = new Random();
+                str += (char)(r.nextInt(26) + 'a');
+            }
+            return str;
+        };
+
+        System.out.println(multiCall(greetingLambda, createNameLambda, "from lambda"));
+
+        // インターフェースのインスタンスを使用して関数への参照を渡す
+        Callable g = new Greet();
+        System.out.println(multiCall(g, createNameLambda, "from normal function"));
+    }
+}
